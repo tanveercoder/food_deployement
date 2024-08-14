c16cf08441f53e91633cba7d1d4b56b82dc5c3a2
tanveercoder <142831424+tanveercoder@users.noreply.github.com>
2024-08-15 02:50:31 +0530

    Update Signup.jsx

--- frontend/src/screen/Signup.jsx
+++ frontend/src/screen/Signup.jsx
@@ -13,7 +13,7 @@ const Signup = () => {
     e.preventDefault();
   
     try {
-      const response = await fetch("http://localhost:5000/api/createuser", {
+      const response = await fetch("https://food-deployement-1.onrender.com/api/createuser", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
